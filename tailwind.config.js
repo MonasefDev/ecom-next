80fc6008b37522d70a6cf8961a10e5c951e12798
monasefdev <monasef77@gmail.com>
2024-10-08 13:16:27 +0100

    setup project

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,38 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "blue-dark": "#0D6EFD",
+        "blue-light": "#E3F0FF",
+        "green-blue": "#55BDC3",
+        "green-dark": "#00B517",
+        "green-light": "#C3FFCB",
+        "orange-darker": "#FF6F00",
+        "orange-dark": "#FF9017",
+        "orange-light": "#FFE5BF",
+        red: "#FA3434",
+        "red-light": "#FFE3E3",
+        white: "#ffffff",
+        black: "#000000",
+        gray: {
+          100: "#F7FAFC",
+          200: "#EFF2F4",
+          300: "#DEE2E7",
+          400: "#BDC4CD",
+          500: "#8B96A5",
+          600: "#505050",
+          dark: "#1C1C1C",
+        },
+      },
+      spacing: {
+        "navbar-desktop": "8.6rem",
+        "navbar-mobile": "5.6rem",
+      },
+      maxWidth: {
+        "screen-xl": "1200px",
+      },
+      screens: {
+        tablet: "1024px",
+        mobile: "768px",
       },
     },
   },
